10866de7534590cf9a498529519a461978be26bb
Ching-Panmei <85636633+Ching-Panmei@users.noreply.github.com>
2021-08-26 02:55:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,21 @@ var slideIndex = 1;
           slides[slideIndex-1].style.display = "block";  
           dots[slideIndex-1].className += " active";
         }
+
+// COUNTER SECTION
+const counters = document.querySelectorAll('.counter');
+const speed = 200;
+counters.forEach( counter=> {
+    const updateCount = () => {
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+        const inc = target / speed;
+        if(count < target) {
+            counter.innerText = Math.ceil(count + inc);
+            setTimeout(updateCount, 1);
+        } else {
+            count.innerText = target;
+        }
+    }
+    updateCount();
+})
